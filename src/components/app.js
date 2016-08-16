cec905c8b7396c4606c86d676f4c686e4b2a230e
Ross Bower <ross@ross-mbp.wireless.dartmouth.edu>
2016-08-16 15:54:23 -0400

    progress

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import Immutable from 'immutable';
 // import Welcome from './welcome';
 import InputBar from './inputbar';
 import Note from './note';
-import * as firebase from '../firebase';
+// import * as firebase from '../firebase';
+import io from 'socket.io-client';
+const socketserver = 'http://localhost:9090';
 
 // example class based component (smart component)
 class App extends Component {
@@ -15,34 +17,40 @@ class App extends Component {
       notes: Immutable.Map(),
     };
 
+    this.socket = io(socketserver);
+    this.socket.on('connect', () => { console.log('socket.io connected'); });
+    this.socket.on('disconnect', () => { console.log('socket.io disconnected'); });
+    this.socket.on('reconnect', () => { console.log('socket.io reconnected'); });
+    this.socket.on('error', (error) => { console.log(error); });
+
     this.addNote = this.addNote.bind(this);
     this.deleteNote = this.deleteNote.bind(this);
     this.updateNote = this.updateNote.bind(this);
   }
 
   componentDidMount() {
-    firebase.fetchNotes((snapshot) => this.setState({ notes: Immutable.Map(snapshot.val()) }));
+    this.socket.on('notes', (snapshot) => this.setState({ notes: Immutable.Map(snapshot.val()) }));
   }
 
   addNote(id, title) {
     // this.setState({
     //   notes: this.state.notes.set(id, { title, x: ((id + 1) * 20), y: ((id + 1) * 20) }),
     // });
-    firebase.addNote({ title, x: ((id + 1) * 20), y: ((id + 1) * 20) });
+    this.socket.emit('createNote', title);
   }
 
   deleteNote(id) {
     // this.setState({
     //   notes: this.state.notes.delete(id),
     // });
-    firebase.deleteNote(id);
+    this.socket.emit('deleteNote', id);
   }
 
   updateNote(id, fields) {
     // this.setState({
     //   notes: this.state.notes.update(id, (n) => { return Object.assign({}, n, fields); }),
     // });
-    firebase.updateNote(id, fields);
+    this.socket.emit('updateNote', id, fields);
   }
 
   render() {
